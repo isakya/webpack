f3dd6474a063522f8cf0f2c6fe0a2acf6dcb1221
konglong <1016361268@qq.com>
2022-06-02 02:50:33 +0800

    提取css为单独文件，并调整相关路径

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,13 +3,19 @@
 // 该文件要符合cjs模块化规范
 
 // 引入node中一个内置的path模块，专门用于解决路径问题
-var path = require('path')
+const path = require('path')
 
-// 引入插件 用于加工html文件
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// 引入插件 用于加工html文件 // 用之前不要style-loader 不然无法提取出css文件
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// 引入mini-css-extract-plugin，用于提取CSS为单独的文件
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 // 基本css-loader的配置
-const baseCssLoader = ['style-loader', 'css-loader']
+const baseCssLoader = [MiniCssExtractPlugin.loader, 'css-loader']
+
+
 
 // 使用cjs的模块化规范，暴露一个对象，该对象就是webpack的详细配置对象(规则)
 module.exports = {
@@ -18,7 +24,9 @@ module.exports = {
   entry: './src/js/app.js', // 入口
   output: {
     path: path.resolve(__dirname, '../build'), // 输出文件的路径
-    filename: 'js/app.js' // 输出文件名字
+    filename: 'js/app.js', // 输出文件名字
+    publicPath: '/build/'
+
   },
   // module.rules 中配置的一个个的loader
   module: {
@@ -26,14 +34,7 @@ module.exports = {
       // 1、配置解析css loader
       { // rules 下一个对象就是 一个loader
         test: /\.css$/, // 匹配规则 匹配所有以.css结尾的文件
-        use: [ // 后指定的loader先执行
-          'style-loader', // 创建style标签，将样式资源插入进，添加到head中生效
-          'css-loader' // 将css 文件变成commonjs模块加载到js中，里面内容是样式字符串
-
-          // 完整版写法 可以对loader做出更详细的配置
-          // {loader: style-loader}
-          // {loader: css-loader}
-        ]
+        use: [...baseCssLoader]
       },
       // 2、配置解析less loader
       {
@@ -52,7 +53,7 @@ module.exports = {
             loader: 'url-loader',
             options: {
               outputPath: 'imgs', // 配置图片加工后存在的位置,不要加 /, 不然要配置publicPath
-              // publicPath: '/build/imgs' // 配置图片引入时前缀路径
+              // publicPath: '/build/imgs', // 配置图片引入时前缀路径
               name: '[hash:5].[ext]', // 处理图片名字长 用:数量
               limit: 8 * 1024 // 图片大小小于8KB时 将图片转为base64
             }
@@ -85,6 +86,10 @@ module.exports = {
     // 实例化HtmlWebpackPlugin
     new HtmlWebpackPlugin({
       template: './src/index.html' // 模板的位置
+    }),
+    // 实例化MiniCssExtractPlugin
+    new MiniCssExtractPlugin({
+      filename: 'css/index.css', // 放到指定的文件夹下，并同时以index.css命名
     })
   ]
 }
